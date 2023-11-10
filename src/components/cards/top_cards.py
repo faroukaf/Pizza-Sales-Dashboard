5b69bea676548833bc23b3baeedf5e67a8223a3a
faroukaf <faroukafmsm1@gmail.com>
2023-11-10 14:32:01 +0200

    edit KPIs cards holder to use bootstrap

--- src/components/cards/top_cards.py
+++ src/components/cards/top_cards.py
@@ -1,3 +1,4 @@
+import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 from sqlite3 import Cursor
@@ -11,8 +12,6 @@ def render(app: Dash, data: Cursor, theme: str) -> html.Div:
   create the card that hold the single value card 
   """
 
-  pics_src = 'assets/'
-
   data.execute('''
   SELECT CAST(printf("%.2f",SUM(total_price)) AS REAL)
   AS `Total Revenue` FROM pizza;
@@ -21,7 +20,7 @@ def render(app: Dash, data: Cursor, theme: str) -> html.Div:
   title1 = data.description[0][0]
   value1 = data.fetchone()[0]
   value1 = str(int(value1/1000)) + 'K'
-  pic1 = pics_src + 'profit-growth.png'
+  icon1 = 'bi bi-cash-stack'
 
   data.execute('''
   SELECT CAST(printf("%.2f",(SUM(total_price) /
@@ -31,7 +30,7 @@ def render(app: Dash, data: Cursor, theme: str) -> html.Div:
 
   title2 = data.description[0][0]
   value2 = data.fetchone()[0]
-  pic2 = pics_src + 'order-delivery.png'
+  icon2 = 'bi bi-card-list'
 
   data.execute('''
   SELECT SUM(quantity) AS `Total Pizza Sold` FROM pizza
@@ -40,7 +39,7 @@ def render(app: Dash, data: Cursor, theme: str) -> html.Div:
   title3 = data.description[0][0]
   value3 = data.fetchone()[0]
   value3 = str(int(value3/1000)) + 'K'
-  pic3 = pics_src + 'delivery-man.png'
+  icon3 = 'bi bi-bicycle'
 
   data.execute('''
   SELECT COUNT(DISTINCT order_id)
@@ -50,7 +49,8 @@ def render(app: Dash, data: Cursor, theme: str) -> html.Div:
   title4 = data.description[0][0]
   value4 = data.fetchone()[0]
   value4 = str(int(value4/1000)) + 'K'
-  pic4 = pics_src + 'order-delivery.png'
+  icon4 = 'bi bi-cart-check-fill'
+
 
   data.execute('''
   SELECT CAST(printf("%.2f",(SUM(quantity) /
@@ -60,15 +60,15 @@ def render(app: Dash, data: Cursor, theme: str) -> html.Div:
 
   title5 = data.description[0][0]
   value5 = data.fetchone()[0]
-  pic5 = pics_src + 'test.png'
+  icon5 = 'fa-solid fa-pizza-slice mt-2 mb-3'
 
-  return html.Div(
+  return dbc.Row(
     children=[
-      single_value_card.render(app, value1, title1, pic1, theme),
-      single_value_card.render(app, value2, title2, pic2, theme),
-      single_value_card.render(app, value3, title3, pic3, theme),
-      single_value_card.render(app, value4, title4, pic4, theme),
-      single_value_card.render(app, value5, title5, pic5, theme),
+      single_value_card.render(app, value1, title1, icon1, theme),
+      single_value_card.render(app, value2, title2, icon2, theme),
+      single_value_card.render(app, value3, title3, icon3, theme),
+      single_value_card.render(app, value4, title4, icon4, theme),
+      single_value_card.render(app, value5, title5, icon5, theme),
     ],
-    className=classes_names.SINGLE_VALUE_CARDS+theme
+    className='d-flex align-items-stretch'
   )
