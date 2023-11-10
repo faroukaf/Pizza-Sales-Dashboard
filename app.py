05a99686f40167ea3d2b6a3269229a1d4385ed52
faroukaf <faroukafmsm1@gmail.com>
2023-11-10 11:08:12 +0200

    use bootstrap

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dash import Dash
-
+import dash_bootstrap_components as dbc
 from src.components import create_layout
 from src.utilities import connector
 # from .src.components.create_layout import render as create_layout
@@ -9,11 +9,17 @@ def main() -> None:
   The main app
   """
 
-  app = Dash()
+  app = Dash(
+    __name__,
+    external_stylesheets=[
+      dbc.themes.DARKLY,
+      dbc.icons.BOOTSTRAP
+    ]
+  )
   cursor = connector.connect('data/pizza.db')
   app.layout = create_layout.render(app, cursor)
 
-  app.run(port=4000)
+  app.run(port=4000, debug=True)
 
 
 if __name__ == '__main__':
