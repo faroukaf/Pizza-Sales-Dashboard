0514d90763d6c448d7ac2750c35020ba762eecd9
faroukaf <faroukafmsm1@gmail.com>
2023-11-12 20:20:30 +0200

    create multi page layout

--- src/components/create_layout.py
+++ src/components/create_layout.py
@@ -1,25 +1,49 @@
-from dash import Dash, dcc, html
+from dash import (
+  Dash,
+  dcc, html,
+  page_container, page_registry
+)
 from sqlite3 import Cursor
+import dash_bootstrap_components as dbc
 from . import page1, page2, swap_pages
+from .cards import top_cards
 from .cards import top_cards
 from ..utilities import classes_names, swap_pages_logic
 
-THEME = 'dark'
 
-
-def render(app: Dash, cursor: Cursor) -> html.Div:
-  '''(Dash) -> Div
+def render(app: Dash, cursor: Cursor) -> dbc.Container:
+  '''(Dash) -> Container
   Create the layout of the app
   '''
 
   # swap_pages_logic.swap(app)
 
-  return html.Div(
-    children=[
-      page1.render(app, cursor, THEME),
-      page2.render(app, cursor, THEME),
-      # swap_pages.render(app, cursor, THEME)
-      # months_checklist.render()
-    ],
-    className= classes_names.MAIN_LAYOUT + THEME
+  return dbc.Container(
+    [
+      dbc.Row(
+        [
+          # Top bar for filter and language and theme
+        ]
+      ),
+      html.Br(),
+      dbc.Row(
+      [
+        dbc.Col(
+          [
+            top_cards.render(app, cursor),
+            page_container
+
+          ],
+          width=10
+        ),
+        dbc.Col(
+          [
+            # Navigation buttons,
+            # Inside container
+          ],
+          width=2
+        )
+      ]
+    )
+    ]
   )
