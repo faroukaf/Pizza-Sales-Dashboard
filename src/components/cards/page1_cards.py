cb2e2c352a217d52b05d7de6bd6802ce4b30cff0
faroukaf <faroukafmsm1@gmail.com>
2023-11-12 20:30:11 +0200

    make page 1 layout

--- src/components/cards/page1_cards.py
+++ src/components/cards/page1_cards.py
@@ -10,29 +10,10 @@ from . import (
 from ...utilities import classes_names, ids
 
 
-def render(app: Dash, cursor: Cursor, theme: str) -> dbc.Col:
+def render(cursor: Cursor) -> dbc.Col:
   '''(Dash) -> Col
-  Create collections of carts in page 1 layout of the app
+  Create collections of cards in page 1 layout of the app
   '''
-
-  # @app.callback(
-  #     [
-  #       Output(ids.SWAP_CONTAINER, 'style'),
-  #       Output(ids.PAGE1, 'hidden'),
-  #       Output(ids.PAGE2, 'hidden'),
-  #       # Output(ids.RIGHT_PAGE_SWAP, 'disable_n_clicks')
-  #     ],
-  #     [
-  #       Input(ids.SWAP_CONTAINER, 'style'),
-  #       Input(ids.RIGHT_PAGE_SWAP, 'n_clicks')
-  #     ]
-  # )
-  # def do_right(style: dict, _: int) -> (bool, bool, bool):
-  #   print(style)
-  #   if style['page-number'] == 1:
-  #     return {'page-number': 2}, True, False#, True
-  #   return {'page-number': 1}, False, True#, False
-
 
   return dbc.Col(
     children=[
@@ -50,14 +31,5 @@ def render(app: Dash, cursor: Cursor, theme: str) -> dbc.Col:
           category_funnel.render(cursor, 'Total Pizza Soled by Pizza Category')
         ]
       ),
-      # html.Br(),
-      # dbc.Row(
-      #   children=[
-      #     # category_pie.render(cursor, '% of Sale by Category'),
-      #     # size_pie.render(cursor, '% of Sale by Size'),
-      #     category_funnel.render(cursor, 'Total Pizza Soled by Pizza Category')
-      #   ]
-      # )
     ],
-    className='w-70'
   )
