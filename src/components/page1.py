3e95c95f31c414f22cd1ae3c241e97688b215f08
faroukaf <faroukafmsm1@gmail.com>
2023-11-12 20:37:10 +0200

    add documentation and remove unwanted imports

--- src/components/page1.py
+++ src/components/page1.py
@@ -1,9 +1,9 @@
+'''
+Create the page 1 "Home" layout of the app
+'''
 import dash_bootstrap_components as dbc
-from dash import Dash, dcc, html, register_page
-from dash.dependencies import Input, Output
-from sqlite3 import Cursor
-from .cards import top_cards, page1_cards
-from ..utilities import classes_names, ids
+from dash import register_page
+from .cards import page1_cards
 
 
 register_page(__name__, path='/')
