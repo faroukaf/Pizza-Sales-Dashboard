4979690e8af5bf43595da6c6a2f133f270aabd6d
faroukaf <faroukafmsm1@gmail.com>
2023-11-13 16:42:18 +0200

    trying edit so work

--- app.py
+++ app.py
@@ -4,25 +4,28 @@ from src.components import create_layout
 from src.utilities import connector
 # from .src.components.create_layout import render as create_layout
 
+
+app = Dash(
+  __name__,
+  use_pages=True,
+  external_stylesheets=[
+    dbc.themes.DARKLY,
+    dbc.icons.BOOTSTRAP,
+    dbc.icons.FONT_AWESOME
+  ]
+)
+LOCALE = 'en'
+cursor = connector.connect('data/pizza.db')
+app.layout = create_layout.render(app, cursor)
+
+app.run(port=4000, debug=True)
+
 def main() -> None:
   """
   The main app
   """
 
-  LOCALE = 'en'
-  app = Dash(
-    __name__,
-    use_pages=True,
-    external_stylesheets=[
-      dbc.themes.DARKLY,
-      dbc.icons.BOOTSTRAP,
-      dbc.icons.FONT_AWESOME
-    ]
-  )
-  cursor = connector.connect('data/pizza.db')
-  app.layout = create_layout.render(app, cursor)
-
-  app.run(port=4000, debug=True)
+  pass
 
 
 if __name__ == '__main__':
--- src/components/page1.py
+++ src/components/page1.py
@@ -4,11 +4,26 @@ Create the page 1 "Home" layout of the app
 import dash_bootstrap_components as dbc
 from dash import register_page
 from .cards import page1_cards
+from src.utilities import connector
 
 
-register_page(__name__, path='/')
+
+for i in range(100000):
+    1+1
 
 
-layout = dbc.Row(
-  page1_cards.render(cursor)
-)
+def w():
+  for i in range(100000):
+    1+1
+
+  cursor = connector.connect('data/pizza.db')
+
+  register_page(__name__)#, path='/')
+
+
+  return dbc.Row(
+    page1_cards.render(cursor)
+  )
+
+
+layout = w()
